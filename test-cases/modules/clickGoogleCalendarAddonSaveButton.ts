d2307ed4354dab32d396cccb0d49b09445e380a8
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2023-01-08 00:52:48 +0900

    chore: deno fmt

--- test-cases/modules/clickGoogleCalendarAddonSaveButton.ts
+++ test-cases/modules/clickGoogleCalendarAddonSaveButton.ts
@@ -3,7 +3,7 @@ import { Page } from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
 const SAVE_BUTTON = "div:nth-child(4) > button";
 
 const clickGoogleCalendarAddonSaveButton = async (
-  page: Page
+  page: Page,
 ): Promise<void> => {
   await page.waitForSelector(SAVE_BUTTON);
   await Promise.all([
--- test-cases/modules/createZoomMeetingUsingChromeExtension.ts
+++ test-cases/modules/createZoomMeetingUsingChromeExtension.ts
@@ -3,7 +3,7 @@ import { Page } from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
 const SAVE_BUTTON = "div.BTotkb.JaKw1 > div:nth-child(4) > div";
 
 const createZoomMeetingUsingChromeExtension = async (
-  page: Page
+  page: Page,
 ): Promise<void> => {
   await page.waitForSelector(SAVE_BUTTON);
   await Promise.all([page.waitForNavigation(), page.click(SAVE_BUTTON)]);
