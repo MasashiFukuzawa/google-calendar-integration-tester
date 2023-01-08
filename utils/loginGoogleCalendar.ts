8bc4d76ec4ab0498b7b51c738e2c4542ed26f156
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2023-01-08 21:22:16 +0900

    fix: loginGoogleCalendar module

--- utils/loginGoogleCalendar.ts
+++ utils/loginGoogleCalendar.ts
@@ -7,11 +7,11 @@ const GOOGLE_PASSWORD_INPUT = 'input[type="password"]';
 const GOOGLE_PASSWORD_NEXT_BUTTON = "#passwordNext";
 const GOOGLE_SMART_PHONE_IMAGE = "#view_container";
 
-const loginToGoogleCalendar = async (page: Page): Promise<void> => {
+const loginGoogleCalendar = async (page: Page): Promise<void> => {
   console.log("Start to login Google Calendar.");
 
   await page.waitForSelector(GOOGLE_EMAIL_INPUT);
-  await page.type(GOOGLE_EMAIL_INPUT, env["EMAIL"]);
+  await page.type(GOOGLE_EMAIL_INPUT, env["GOOGLE_EMAIL"]);
 
   await page.waitForSelector(GOOGLE_EMAIL_NEXT_BUTTON);
   await Promise.all([
@@ -20,7 +20,7 @@ const loginToGoogleCalendar = async (page: Page): Promise<void> => {
   ]);
 
   await page.waitForSelector(GOOGLE_PASSWORD_INPUT, { visible: true });
-  await page.type(GOOGLE_PASSWORD_INPUT, env["PASSWORD"]);
+  await page.type(GOOGLE_PASSWORD_INPUT, env["GOOGLE_PASSWORD"]);
 
   await page.waitForSelector(GOOGLE_PASSWORD_NEXT_BUTTON, { visible: true });
   await Promise.all([
@@ -31,9 +31,9 @@ const loginToGoogleCalendar = async (page: Page): Promise<void> => {
   console.log("Please complete two-factor authentication manually.");
   await page.waitForSelector(GOOGLE_SMART_PHONE_IMAGE);
 
-  await page.waitForNavigation();
+  await page.waitForNavigation({ timeout: 1000 * 60 * 3 }); // within 3min
 
   console.log("Successful login to Google Calendar.");
 };
 
-export default loginToGoogleCalendar;
+export default loginGoogleCalendar;
